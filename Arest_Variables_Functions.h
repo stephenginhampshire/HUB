f6a5b691a3ace321348463c44d13a6a603c0a5e5
Stephen Gould <stephen_gould@hotmail.com>
2018-12-30 12:54:52 +0000

    Initial

---
+++ Arest_Variables_Functions.h
@@ -0,0 +1,57 @@
+#ifdef REST
+// Expose variables to rest --------------------------------------------------------------------------------------------------------------
+  {
+  rest.set_id("001");                 // Give ID to the device
+  rest.set_name("Stephen&Jamie");
+  rest.variable("Altitude", &Altitude_value);											// v1
+  rest.variable("AtHome", &AtHome_value);												// v2
+  rest.variable("AtPark", &AtPark_value);												// v3
+  rest.variable("Azimuth", &Azimuth_value);											// v4
+  rest.variable("Slewing", &Slewing_value);											// v5
+  rest.variable("TargetAltitude", &TargetAltitude_value);								// v6
+  rest.variable("TargetAzimuth", &TargetAzimuth_value);								// v7
+  rest.variable("Tracking", &Tracking_value);											// v8
+  rest.variable("TrackingRateAzimuth", &TrackingRateAzimuth_value);					// v9
+  rest.variable("TrackingRateAltitude", &TrackingRateAltitude_value);					// v10
+  rest.variable("HubDisplayStatus", &hub_display_state);								// v11
+  rest.variable("FocuserDisplayStatus", &Focuser_display_state);						// v12
+  rest.variable("FocuserCurrentPosition", &Focuser_Current_Position_value);			// fv1
+  rest.variable("FocuserCurrentTemperature", &Focuser_Current_Temperature_value);		// fv2
+  rest.variable("FocuserCurrentHumidity", &Focuser_Current_Humidity_value);			// fv3
+  rest.variable("FocuserLatitude", &Focuser_Latitude_value);							// fv4
+  rest.variable("FocuserLongitude", &Focuser_Longitude_value);						// fv5
+  rest.variable("FocuserAltitude", &Focuser_Altitude_value);							// fv6
+  rest.variable("FocuserYear", &Focuser_Year_value);									// fv7
+  rest.variable("FocuserMonth", &Focuser_Month_value);								// fv8
+  rest.variable("FocuserDay", &Focuser_Day_value);									// fv9
+  rest.variable("FocuserHour", &Focuser_Hour_value);									// fv10
+  rest.variable("FocuserMinute", &Focuser_Minute_value);								// fv11
+  rest.variable("FocuserSecond", &Focuser_Second_value);								// fv12
+  }
+  // ASCOM functions exposed to rest
+  {
+	  rest.function("TargetAltitude", TargetAltitude_f);							// fa2
+	  rest.function("TargetAzimuth", TargetAzimuth_f);							// fa3
+	  rest.function("Tracking", Tracking_f);										// fa4
+	  rest.function("TrackingRates", TrackingRates_f);							// fa5
+	  rest.function("AbortSlew", AbortSlew_f);									// fa6
+	  rest.function("FindHome", FindHome_f);										// fa7
+	  rest.function("Park", Park_f);												// fa8
+	  rest.function("SlewToAltAz", SlewToAltAz_f);								// fa9
+	  rest.function("SlewToTarget", SlewToTarget_f);								// fa10
+	  rest.function("SyncToAltAz", SyncToAltAz_f);								// fa11
+	  rest.function("PulseGuide", PulseGuide_f);									// fa12
+	  rest.function("GuideRateRightAscension", GuideRateRightAscension_f);		// fa13
+	  rest.function("GuideRateDeclination", GuideRateDeclination_f);				// fa14
+	  rest.function("UnPark", UnPark_f);											// fa15
+	  rest.function("HubDisplay_On", Hub_Display_On_f);							// fa16
+	  rest.function("HubDisplay_Off", Hub_Display_Off_f);							// fa17
+	  rest.function("FocuserHome", Focuser_Home_f);								// ff1
+	  rest.function("FocuserMove", Focuser_Move_f);								// ff2
+	  rest.function("FocuserMove_to", Focuser_Move_to_f);							// ff3
+	  rest.function("FocuserDisplay_On", Focuser_Display_On_f);					// ff4
+	  rest.function("FocuserDisplay_Off", Focuser_Display_Off_f);					// ff5
+	  rest.function("FocuserHalt", Focuser_Halt_f);								// ff6
+  }
+  //--------------------------------------------------------------------------------------------------------------------------------------
+#endif
---
+++ HUB_Commands.h
@@ -0,0 +1,43 @@
+/* 
+ * File:   HUB_Commands.h
+ * Author: Stephen
+ *
+ * Created on 14 February 2018, 13:19
+ */
+
+#ifndef HUB_COMMANDS_H
+#define	HUB_COMMANDS_H
+
+#define AZ_Home_command                 0x10        // i1   special HOME command from ALT buttob pad to AZ motor
+#define AZ_Halt_command                 0x12        // i2   special HALT command from ALT button pad to AZ motor
+#define Halt_command                    0x14        // i3   Motor emergency halt
+#define Azimuth_Display_command         0x16        // i4   packet containing Azimuth Bearing 
+#define Motor_update_command            0x05        // c1
+#define TargetAltitude_command          0xA0        // c2
+#define TargetAzimuth_command           0xA2        // c3
+#define Tracking_command				0xA4        // c4
+#define TrackingRates_command           0xA6        // c5
+#define AbortSlew_command				0xB0        // c6
+#define FindHome_command				0xB2        // c7
+#define Park_command					0xB4        // c8
+#define SlewToAltAz_command				0xC0        // c9
+#define SlewToTarget_command			0xC8        // c10
+#define SyncToAltAz_command				0xD0        // c11
+#define PulseGuide_command              0xE0        // c12
+#define GuideRateRightAscension_command 0xE2        // c13
+#define GuideRateDeclination_command    0xE4        // c14
+#define UnPark_command					0xB6        // c15
+
+#define Hub_Display_On_command			0x30
+#define Hub_Display_Off_command			0x32
+
+#define Focuser_Update_command			0x05		// fc1
+#define Focuser_Home_command			0x10		// fc2
+#define Focuser_Move_command			0x20		// fc3
+#define Focuser_Move_to_command			0x25		// fc4
+#define Focuser_Display_On_command		0x30		// fc5
+#define Focuser_Display_Off_command		0x35		// fc6
+#define Focuser_Halt_command			0xFF		// fc7
+
+#endif	/* HUB_COMMANDS_H */
+
